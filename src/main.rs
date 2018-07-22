d06f4e2dbf38a55ee85970df850a1a971e300a8f
Hampus Gunnarsson <takeoutguhui@gmail.com>
2018-07-22 13:54:42 +0200

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,36 @@ extern crate rand;
 extern crate regex;
 
 use std::io;
-use rand::prelude::*;
+use rand::{Rng, thread_rng};
 use regex::Regex;
 
 fn main() {
-     
+    let mut rng = thread_rng();
+
     loop {
         let mut line = String::new();
         io::stdin().read_line(&mut line).expect("couldn't read line");
+        let line = line.trim();
 
         let re = Regex::new(r"^\d+d\d+$").unwrap();
-        if !re.is_match(&line.trim()) {
+        if !re.is_match(&line) { 
             println!("not the right format");
             continue;
         }
 
         let split: Vec<&str> = line.split("d").collect();
-        let num_rolls: u32 = split[0].trim().parse().unwrap();
-        let num_sides: u32 = split[1].trim().parse().unwrap();
-
-        let mut rng = thread_rng();
+        let num_rolls: u32 = split[0].parse().unwrap(); 
+        let num_sides: u32 = split[1].parse().unwrap();
+        
         let rolls: Vec<u32> = (0..num_rolls)
-                        .map(|_| rng.gen_range(1, num_sides + 1)).collect();
+                        .map(|_| rng.gen_range(1, num_sides + 1))
+                        .collect();
 
-        let rolls_string: String = rolls.iter()
-                        .map(|roll| roll.to_string())
-                        .collect::<Vec<String>>()
-                        .join(" ");
-        println!("{}: {}", rolls.iter().sum::<u32>(), rolls_string);
+        println!("{}: {:?}", rolls.iter().sum::<u32>(), rolls);
     }
 }
+
+//let rolls_as_string: String = rolls.iter()
+//                .map(|roll| roll.to_string())
+//                .collect::<Vec<String>>()
+//                .join(" ");
